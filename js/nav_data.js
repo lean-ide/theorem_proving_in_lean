de2ba75934129e607c512dd3ff0bc8fa4c308c44
Adam Kurkiewicz <adam@kurkiewicz.pl>
2017-02-02 16:49:26 +0000

    Update Thu Feb  2 16:49:26 GMT 2017

---
+++ js/nav_data.js
@@ -0,0 +1,1 @@
+var lean_nav_data=["01_Introduction.html","02_Dependent_Type_Theory.html","03_Propositions_and_Proofs.html","04_Quantifiers_and_Equality.html","05_Tactics.html","06_Interacting_with_Lean.html","07_Inductive_Types.html","08_Induction_and_Recursion.html","09_Structures_and_Records.html","10_Type_Classes.html"];
